cbba7ee3b1bb5f4c788a183db0f040d03db0b55a
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2016-10-12 18:49:12 +0200

    Add a 'Test Connection' button to the Omero Connection node

--- org.knime.knip.newomero/src/org/knime/knip/newomero/nodes/connection/OmeroConnectionInfoNodeDialog.java
+++ org.knime.knip.newomero/src/org/knime/knip/newomero/nodes/connection/OmeroConnectionInfoNodeDialog.java
@@ -1,11 +1,21 @@
 package org.knime.knip.newomero.nodes.connection;
 
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.knime.base.filehandling.remote.connectioninformation.node.TestConnectionDialog;
+import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
 import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
+import org.knime.core.node.defaultnodesettings.DialogComponentButton;
 import org.knime.core.node.defaultnodesettings.DialogComponentLabel;
 import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
 import org.knime.core.node.defaultnodesettings.DialogComponentPasswordField;
@@ -14,6 +24,9 @@ import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
 import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
 import org.knime.core.node.defaultnodesettings.SettingsModelInteger;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.workflow.CredentialsProvider;
+import org.knime.core.node.workflow.ICredentials;
+import org.knime.knip.newomero.port.OmeroConnectionInformation;
 
 public class OmeroConnectionInfoNodeDialog extends DefaultNodeSettingsPane {
 
@@ -55,10 +68,16 @@ public class OmeroConnectionInfoNodeDialog extends DefaultNodeSettingsPane {
 		addDialogComponent(credentialsComponent);
 		errorLabel = new DialogComponentLabel("");
 
-		createNewGroup("Username Password");
+		createNewGroup("Manual Credentials");
 		addDialogComponent(new DialogComponentString(usernameModel, "username", true, 15));
 		addDialogComponent(new DialogComponentPasswordField(passwordModel, "password"));
 		closeCurrentGroup();
+
+		createNewGroup("");
+
+		DialogComponentButton testConnectionButton = new DialogComponentButton("Test connection");
+		addDialogComponent(testConnectionButton);
+		testConnectionButton.addActionListener(new TestConnectionListener());
 
 	}
 
@@ -85,4 +104,50 @@ public class OmeroConnectionInfoNodeDialog extends DefaultNodeSettingsPane {
 		}
 	}
 
+	private class TestConnectionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(final ActionEvent e) {
+			// Get frame
+			Frame frame = null;
+			Container container = getPanel().getParent();
+			while (container != null) {
+				if (container instanceof Frame) {
+					frame = (Frame) container;
+					break;
+				}
+				container = container.getParent();
+			}
+			try {
+				// Get connection information to current settings
+				final OmeroConnectionInformation connectionInformation = createConnectionInformation(
+						getCredentialsProvider());
+				// Open dialog
+				new TestConnectionDialog(connectionInformation).open(frame);
+			} catch (final InvalidSettingsException exc) {
+				JOptionPane.showMessageDialog(new JFrame(), exc.getMessage(), "Invalid settings",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
+
+		// create temporary connection information for use in the test
+		// connection method
+		private OmeroConnectionInformation createConnectionInformation(CredentialsProvider credentialsProvider)
+				throws InvalidSettingsException {
+			OmeroConnectionInformation info = new OmeroConnectionInformation();
+			info.setProtocol("ome");
+			info.setHost(hostnameModel.getStringValue());
+			info.setPort(portModel.getIntValue());
+			info.setUseEncryption(encryptModel.getBooleanValue());
+			if (useCredentialsModel.getBooleanValue()) {
+				ICredentials creds = credentialsProvider.get(credentialsModel.getStringValue());
+				info.setUser(creds.getLogin());
+				info.setPassword(creds.getPassword());
+			} else {
+				info.setUser(usernameModel.getStringValue());
+				info.setPassword(passwordModel.getStringValue());
+			}
+			return info;
+		}
+	}
 }
